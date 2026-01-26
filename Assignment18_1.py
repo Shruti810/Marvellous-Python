930dea087c99fbc5b25574ac1b130bbaa6b7ce12
Shruti810 <71802925+Shruti810@users.noreply.github.com>
2026-01-26 22:01:02 +0530

    Add files via upload

---
+++ Assignment18_1.py
@@ -0,0 +1,17 @@
+def Addition(lst):
+    total = 0
+
+    for No in lst:
+        value = int(input())
+        total = total + No
+    return total
+
+num = int(input("number of elements: "))
+number = []
+
+for i in range(num):
+    n = int(input("Enter element:"))
+    number.append(n)
+
+Result = Addition(number)
+print("Output:",Result)
